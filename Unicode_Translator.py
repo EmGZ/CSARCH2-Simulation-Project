43d664a3de655e791c5aaff1f913b3e1ac74a444
Mary Joselle M. Cabungcal <mary_joselle_cabungcal@dlsu.edu.ph>
2024-03-23 04:58:30 +0800

    Integrated Functions
    - Updated the save_to_file function
    - Updated the convertion function
    - Updated the hex_to_utf function
    - Updataed the utf_to_hex function

--- Unicode_Translator.py
+++ Unicode_Translator.py
@@ -1,33 +1,37 @@
 import tkinter as tk
+import textwrap as tw
 from tkinter import ttk, filedialog, messagebox
 
-def hex_to_utf(utf_input, conversion_type):
+def hex_to_utf(utf_input):
     try:
         decimal_value = int(utf_input, 16)
-        if conversion_type == "Hex to Utf-8":
-            utf_result = chr(decimal_value).encode('utf-8').hex().upper()
-        elif conversion_type == "Hex to Utf-16":
-            utf_result = chr(decimal_value).encode('utf-16be').hex().upper()
-        elif conversion_type == "Hex to Utf-32":
-            utf_result = chr(decimal_value).encode('utf-32be').hex().upper()
+   
+        utf_result = []
+        utf8 = chr(decimal_value).encode('utf-8').hex().upper()
+        utf16 = chr(decimal_value).encode('utf-16be').hex().upper()
+        utf32 = chr(decimal_value).encode('utf-32be').hex().upper()
+        utf_result.append(' '.join(tw.wrap(utf8, 2)))  # UTF-8
+        utf_result.append(' '.join(tw.wrap(utf16, 2)))  # UTF-16
+        utf_result.append(' '.join(tw.wrap(utf32, 2)))  # UTF-32
         
         return utf_result
+    
     except ValueError:
         return None
 
 def utf_to_hex(hex_input, conversion_type):
     try:
-        if conversion_type == "Utf-8 to Hex":
+        if conversion_type == "Utf-8":
             hex_result = hex(ord(bytes.fromhex(hex_input).decode('utf-8'))).upper()
-        elif conversion_type == "Utf-16 to Hex":
+        elif conversion_type == "Utf-16":
             hex_result = hex(ord(bytes.fromhex(hex_input).decode('utf-16be'))).upper()
-        elif conversion_type == "Utf-32 to Hex":
+        elif conversion_type == "Utf-32":
             hex_result = hex(ord(bytes.fromhex(hex_input).decode('utf-32be'))).upper()
         
         return hex_result
     except ValueError:
         return None
-def validate_hex_input(text, encoding):
+def validate_hex_input(text):
     if text == '':
         return False  # Do not allow empty string
     if text.startswith('0x') or text.startswith('0X'):
@@ -35,12 +39,8 @@ def validate_hex_input(text, encoding):
       
     hex_value = int(text, 16)
 
-    if encoding == 'utf-8':
-        return hex_value <= 0x1FFFFF and all(c.upper() in '0123456789ABCDEF' for c in text)
-    elif encoding == 'utf-16':
-        return hex_value <= 0x10FFFF and all(c.upper() in '0123456789ABCDEF' for c in text)
-    elif encoding == 'utf-32':
-        return all(c.upper() in '0123456789ABCDEF' for c in text) and len(text) <= 8
+    if hex_value <= 0x1FFFFF and all(c.upper() in '0123456789ABCDEF' for c in text) and hex_value <= 0x10FFFF and all(c.upper() in '0123456789ABCDEF' for c in text) and all(c.upper() in '0123456789ABCDEF' for c in text) and len(text) <= 8:
+        return True
     else:
         return False  # Invalid encoding specified
     
@@ -49,32 +49,19 @@ def validate_utf_input(text):
         return False  # Do not allow empty string
     return all(c.upper() in '0123456789ABCDEF' for c in text) and len(text) <= 8
 
-def convert_input():
-    result_text = process_input(True)
-    if result_text is not None:
-        save_to_file(result_text)
+def convert_input(inputtype):
+    result_text = process_input(inputtype)
 
-def translate_input():
-    result_text = process_input(False)
-    if result_text is not None:
-        save_to_file(result_text)
-
-def process_input(isConvert):
+def process_input(inputtype):
     input_value = entry_input.get().upper()
-  
-    if isConvert:
-        # "Hex to Utf-8", "Hex to Utf-16", "Hex to Utf-32"
-        if conversion_var.get() == "Hex to Utf-8":
-            encoding = 'utf-8'
-        elif conversion_var.get() == "Hex to Utf-16":
-            encoding = 'utf-16'
-        elif conversion_var.get() == "Hex to Utf-32":
-            encoding = 'utf-32'
-        # Convert hexadecimal text to integer
-        if validate_hex_input(input_value, encoding):
-            utf_result = hex_to_utf(input_value, conversion_var.get())
+
+    if inputtype == "Hex":
+        if validate_hex_input(input_value):
+            utf_result = hex_to_utf(input_value)
             if utf_result is not None:
+                input_value = "U+" + input_value
                 add_to_history(input_value, utf_result)
+                entry_input.delete(0, 'end')  # Clear the input field
                 return utf_result
             else:
                 messagebox.showerror("Error", "can not convert!")
@@ -83,18 +70,22 @@ def process_input(isConvert):
             messagebox.showerror("Error", "Invalid input! Please enter a valid hexadecimal value.")
     else:
         if validate_utf_input(input_value):
-            hex_result = utf_to_hex(input_value, translation_var.get())
+            hex_result = utf_to_hex(input_value, conversion_var.get())
             if hex_result is not None:
                 add_to_history(input_value, hex_result)
+                entry_input.delete(0, 'end')  # Clear the input field
                 return hex_result
             else:
                 messagebox.showerror("Error", "Invalid UTF input!")
         else:
             messagebox.showerror("Error", "Invalid input! Please enter a valid UTF value.")
 
+
 def add_to_history(input_value, result):
     history.append((input_value, result))
     update_table()
+    update_rbutton()
+    # print(history)
 
 def update_table():
     table.delete(*table.get_children())
@@ -105,15 +96,37 @@ def update_table():
         table.insert("", "end", values=item)
 
 def reset_table():
-    history.clear()
-    table.delete(*table.get_children())
+    history.clear() # Clear History
+    table.delete(*table.get_children()) # Clear Table
+    update_rbutton()
+    entry_input.delete(0, 'end')  # Clear the input field
 
 
 def save_to_file(result_text):
+    # print(result_text)
     file_path = "output.txt"
-    with open(file_path, "a") as file:  # Open the file in append mode
-        file.write(result_text + "\n")   # Append the new result followed by a newline character
-    messagebox.showinfo("File Updated", "Result appended to {}".format(file_path))
+    with open(file_path, "w") as file:
+        file.write("Input" + "\t\t\t\t\t\t" + "Result" + "\n")
+        for i, j in result_text:
+            if isinstance(j, list):
+                j = ' | '.join(j)  # Handling Multi-value result (Hex to UTFs)
+            file.write(i + "\t\t" + j + "\n")   
+    messagebox.showinfo("File Updated", "Result saved to {}".format(file_path))
+
+def enable_button(*args):
+    if conversion_var.get() or translation_var.get(): 
+        button_translate.config(state="normal") # Enable convert button only if an input type is selected 
+    else:
+        button_translate.config(state="disabled") 
+
+def update_rbutton():
+    if history:
+        button_reset.config(state="normal")
+        button_save.config(state="normal")
+    else:
+        button_reset.config(state="disabled")
+        button_save.config(state="disabled")
+
     
 history = []
 
@@ -129,33 +142,36 @@ label_group.grid(row=1, column=0, columnspan=4, padx=5, pady=5, sticky="nsew")
 entry_input = tk.Entry(window, font=("Helvetica", 10)) 
 entry_input.grid(row=2, column=0, columnspan=4, padx=5, pady=5, sticky="ew")
 
+label_group = tk.Label(window, text="Input Type", font=("Helvetica", 10))
+label_group.grid(row=3, column=0, columnspan=4, padx=5, pady=5, sticky="nsew")
+
 # Radio buttons for conversion type selection
 conversion_var = tk.StringVar()
 
-conversion_choices = ["Hex to Utf-8", "Hex to Utf-16", "Hex to Utf-32"]
-
-for i, choice in enumerate(conversion_choices):
-    radio_button = tk.Radiobutton(window, text=choice, variable=conversion_var, value=choice)
-    radio_button.grid(row=3, column=i, padx=(10, 5), pady=5, sticky="w")
-
-button_convert = tk.Button(window, text="Convert", command=convert_input, width=7) 
-button_convert.grid(row=4, column=1, padx=5, pady=5, sticky="ew")
+radio_button = tk.Radiobutton(window, text="Hex", variable=conversion_var, value="Hex")
+radio_button.grid(row=4, column=0, columnspan=4, padx=5, pady=5, sticky="nsew")
 
 # Radio buttons for translation type selection
 translation_var = tk.StringVar()
 
-translation_choices = ["Utf-8 to Hex", "Utf-16 to Hex", "Utf-32 to Hex"]
+translation_choices = ["Utf-8", "Utf-16", "Utf-32"]
 for i, choice in enumerate(translation_choices):
-    radio_button = tk.Radiobutton(window, text=choice, variable=translation_var, value=choice)
-    radio_button.grid(row=5, column=i, padx=(10, 5), pady=5, sticky="w")
+    radio_button = tk.Radiobutton(window, text=choice, variable=conversion_var, value=choice)
+    radio_button.grid(row=5, column=1+i, padx=(15, 10), pady=5, sticky="w")
 
-button_translate = tk.Button(window, text="Translate", command=translate_input, width=7) 
-button_translate.grid(row=6, column=1, padx=5, pady=5, sticky="ew")
+button_translate = tk.Button(window, text="Convert/Translate", command=lambda: convert_input(conversion_var.get()), width=7, state="disabled") # translate_input
+button_translate.grid(row=6, column=0, columnspan=4, padx=3, pady=5, sticky="ew")
 
 table = ttk.Treeview(window, show="headings")
 table.grid(row=7, column=0, columnspan=4, padx=5, pady=5, sticky="nsew")
 
-button_reset = tk.Button(window, text="Reset", command=reset_table, width=7)
-button_reset.grid(row=8, column=1, padx=5, pady=5, sticky="ew")
+button_reset = tk.Button(window, text="Reset", command=reset_table, width=7, state="disabled")
+button_reset.grid(row=8, column=0, columnspan=4, padx=5, pady=5, sticky="ew")
+
+button_save = tk.Button(window, text="Save Output to File", command=lambda: save_to_file(history), width=7, state="disabled") # Change this to save to file
+button_save.grid(row=9, column=0, columnspan=4, padx=5, pady=5, sticky="ew")
+
+conversion_var.trace_add("write", enable_button) 
+translation_var.trace_add("write", enable_button)  
 
 window.mainloop()
